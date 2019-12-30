6382750f9b3bdd4a2cd4b785883af07a2741eb79
MACorman <madelinecorman@gmail.com>
2019-12-30 17:05:54 -0500

    Login, create user, likes done

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_secure_password
+    
+    has_many :posts
+    has_many :comments
+    has_many :posts, through: :comments
+    has_many :likes
+    has_many :posts, through: :likes
+
+    has_many :active_relationships, class_name: "Obsession", foreign_key: :fan_id, dependent: :destroy
+    has_many :idols, through: :active_relationships, source: :idol
+    has_many :passive_relationships, class_name: "Obsession", foreign_key: :idol_id, dependent: :destroy
+    has_many :fans, through: :passive_relationships, source: :fan
+end
